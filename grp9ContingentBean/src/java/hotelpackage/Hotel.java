09a7001fa0d2ca77d8bf05ee678a9737c7b1e0d4
Kevin <Kevin@193.11.104.98>
2014-11-02 17:26:17 +0100

    test

---
+++ grp9ContingentBean/src/java/hotelpackage/Hotel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelpackage;
+
+/**
+ *
+ * @author Kevin
+ */
+
+import javax.ejb.Remote;
+
+@Remote
+public interface Hotel {
+    
+    
+}
